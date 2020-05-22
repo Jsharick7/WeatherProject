d002867a4fc40dcdb573dcc318a9284a46526dfb
jsharick7 <jsharick7@gmail.com>
2020-05-21 22:55:53 -0400

    added error handler, restyled for smaller screens

--- app.js
+++ app.js
@@ -20,24 +20,26 @@ app.post("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?appid=" + appid + "&units=Imperial&zip=" + query;
   https.get(url, function(response) {
 
+      response.on("data", function(data) {
 
-    response.on("data", function(data) {
-
-      const weatherData = JSON.parse(data);
-      console.log(weatherData);
-      const temp = weatherData.main.temp
-      const description = weatherData.weather[0].description
-      const icon = weatherData.weather[0].icon
-      const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
-      // res.write("<h1> Current: " + description +"</h1>");
-      // res.write("<img src=" + imageURL + ">")
-      // res.write("<h1>The temperature at " + query + " is " + temp + " degrees Farenheit.</h1>");
-      // res.send();
-      res.render("newQuery",{temp:temp, description: description, icon: icon, query:query, imageURL:imageURL});
+        try{
+          const weatherData = JSON.parse(data);
+          console.log(weatherData);
+          const temp = weatherData.main.temp
+          const description = weatherData.weather[0].description
+          const icon = weatherData.weather[0].icon
+          const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+          res.render("newQuery",{temp:temp, description: description, icon: icon, query:query, imageURL:imageURL});
+        }
+        catch(error){
+          console.log(error);
+          res.render("error",{});
+        }
+      });
     });
-  });
+});
 
-})
+
 
 
 
